9fdb74924024610fed4354735d66ac88df698739
cnc-kvsr <kevsercanca@gmail.com>
2021-05-05 01:24:30 +0300

    4.gün 2.ödev

--- Starbucks_Nero/src/Main.java
+++ Starbucks_Nero/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		
 		Customer customer = new Customer();
 		customer.setId(1);
-		customer.setNationalityId("37229058680");
+		customer.setNationalityId("37229058689");
 		customer.setFirstName("Kevser");
 		customer.setLastName("Canca");
 		customer.setDateOfBirth(new GregorianCalendar(1990,1,7).getTime());
